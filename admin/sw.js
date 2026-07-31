d004b637b8d304cd8d61a2772c936d4238a76aff
Jericho Falsario/WPH/Wistron <Jericho_Falsario@wistron.com>
2026-07-31 13:03:31 +0800

    Add local PWA PNG icons and update manifest for Edge PWA installation

--- admin/sw.js
+++ admin/sw.js
@@ -1,18 +1,27 @@
 // Service Worker for Jericho Admin PWA
-const CACHE_NAME = 'jericho-admin-v1';
+const CACHE_NAME = 'jericho-admin-v2';
 const urlsToCache = [
+  './',
   './index.html',
   './css/admin.css',
   './js/admin.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
   '../assets/css/styles.css'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    })
+    }).catch(err => console.log('Cache install error:', err))
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
